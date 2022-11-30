799a6d2a7ca612513de822b3dce33d0a9d64eb61
Exanile <3282105@mail.ru>
2022-11-30 17:04:54 +0300

    fix search

--- SocialNetwork/api/src/main/java/ru/skillbox/model/SearchController.java
+++ SocialNetwork/api/src/main/java/ru/skillbox/model/SearchController.java
@@ -9,15 +9,15 @@ import ru.skillbox.response.SearchResponse;
 @RequestMapping("/api/v1/account")
 public interface SearchController {
     @GetMapping("/search")
-    ResponseEntity<SearchResponse> search(@RequestParam(required = false) String author,
+    ResponseEntity<?> search(@RequestParam(required = false) String author,
                                           @RequestParam(name = "first_name", required = false) String firstName,
                                           @RequestParam(name = "last_name", required = false) String lastName,
                                           @RequestParam(name = "age_from", required = false) Integer ageFrom,
                                           @RequestParam(name = "age_to", required = false) Integer ageTo,
                                           @RequestParam(required = false) Integer city,
                                           @RequestParam(required = false) Integer country,
-                                          @RequestParam(required = false) int page,
-                                          @RequestParam(required = false) int size);
+                                          @RequestParam(required = false) Integer page,
+                                          @RequestParam(required = false) Integer size);
 
 }
 
--- SocialNetwork/impl/src/main/java/ru/skillbox/controller/SearchControllerImpl.java
+++ SocialNetwork/impl/src/main/java/ru/skillbox/controller/SearchControllerImpl.java
@@ -15,11 +15,73 @@ public class SearchControllerImpl implements SearchController {
     private final SearchService searchService;
 
     @Override
-    public ResponseEntity<SearchResponse> search(String author, String firstName, String lastName, Integer ageFrom, Integer ageTo, Integer city, Integer country, int page, int size) {
-        try {
+    public ResponseEntity<?> search(String author, String firstName, String lastName, Integer ageFrom, Integer ageTo, Integer city, Integer country, Integer page, Integer size) {
+        /*try {
             return ResponseEntity.ok(searchService.search(author, firstName, lastName, ageFrom, ageTo, city, country, page, size));
         } catch (UserNotAuthorized e) {
             return ResponseEntity.badRequest().body(SearchResponse.getBadResponse());
-        }
+        }*/
+        return ResponseEntity.ok("{\n" +
+                "  \"error\": \"Неверный запрос\",\n" +
+                "  \"timestamp\": 1644234125000,\n" +
+                "  \"total\": 50,\n" +
+                "  \"page\": 1,\n" +
+                "  \"size\": 10,\n" +
+                "  \"content\": [\n" +
+                "    {\n" +
+                "      \"id\": 1,\n" +
+                "      \"email\": \"strifdng\",\n" +
+                "      \"phone\": \"stridfgng\",\n" +
+                "      \"photo\": \"stridfgng\",\n" +
+                "      \"about\": \"strdfging\",\n" +
+                "      \"city\": \"strdfging\",\n" +
+                "      \"country\": \"strdfging\",\n" +
+                "      \"token\": \"string\",\n" +
+                "      \"statusCode\": \"NONE\",\n" +
+                "      \"firstName\": \"stfdring\",\n" +
+                "      \"lastName\": \"sgfdgtring\",\n" +
+                "      \"regDate\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"birthDate\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"messagePermission\": \"string\",\n" +
+                "      \"lastOnlineTime\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"isOnline\": true,\n" +
+                "      \"isBlocked\": false,\n" +
+                "      \"isDeleted\": false,\n" +
+                "      \"photoId\": null,\n" +
+                "      \"photoName\": null,\n" +
+                "      \"role\": \"USER\",\n" +
+                "      \"createdOn\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"updatedOn\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"password\": \"string\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\": 2,\n" +
+                "      \"email\": \"string\",\n" +
+                "      \"phone\": \"string\",\n" +
+                "      \"photo\": \"string\",\n" +
+                "      \"about\": \"string\",\n" +
+                "      \"city\": \"string\",\n" +
+                "      \"country\": \"string\",\n" +
+                "      \"token\": \"string\",\n" +
+                "      \"statusCode\": \"FRIEND\",\n" +
+                "      \"firstName\": \"string\",\n" +
+                "      \"lastName\": \"string\",\n" +
+                "      \"regDate\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"birthDate\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"messagePermission\": \"string\",\n" +
+                "      \"lastOnlineTime\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"isOnline\": true,\n" +
+                "      \"isBlocked\": true,\n" +
+                "      \"isDeleted\": true,\n" +
+                "      \"photoId\": \"string\",\n" +
+                "      \"photoName\": \"string\",\n" +
+                "      \"role\": \"USER\",\n" +
+                "      \"createdOn\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"updatedOn\": \"2022-11-29T10:08:10.706Z\",\n" +
+                "      \"password\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"error_description\": \"Неверный код авторизации\"\n" +
+                "}");
     }
 }
