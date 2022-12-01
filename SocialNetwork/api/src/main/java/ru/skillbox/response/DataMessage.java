368c2dcb5fc99fb99137079a6c778c78277d24bd
Daniil Epikhov <depikhov29@gmail.com>
2022-12-01 13:33:36 +0300

    DataMessage is added

---
+++ SocialNetwork/api/src/main/java/ru/skillbox/response/DataMessage.java
@@ -0,0 +1,14 @@
+package ru.skillbox.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DataMessage {
+    private String message;
+}
